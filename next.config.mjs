00e937bf49e1fd0198954d783af29f756cf5a9d9
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-04-28 19:53:40 +0530

    updated folder structure, removed conflicts with routers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  // Explicitly disable App Router
+  experimental: {
+    appDir: false
+  }
+};
 
 export default nextConfig;
