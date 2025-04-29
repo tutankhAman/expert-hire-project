c6dad6f6bf7da14d77e34aa3f7cc5c87f7889eff
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-04-29 17:30:47 +0530

    rough homepage layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,77 @@
-import React from 'react';
-import Carousel from '../components/Carousel';
-import ArticleCard from '../components/ArticleCard';
-import Sidebar from '../components/Sidebar';
-import SearchBar from '../components/SearchBar';
-import CategoryFilter from '../components/CategoryFilter';
+import { useState } from 'react';
+import Navbar from '../components/Navbar';
+import HeroCarousel from '../components/sections/HeroCarousel';
+import CategorySection from '../components/sections/CategorySection';
+import RecentArticles from '../components/sections/RecentArticles';
+import Sidebar from '../components/sections/Sidebar';
+import fs from 'fs';
+import path from 'path';
 
-export default function Home() {
+export default function Home({ articles, categories }) {
+  const [selectedCategory, setSelectedCategory] = useState('all');
+
   return (
-    <div className="container">
-      <Carousel />
-      <main>
-        <SearchBar />
-        <CategoryFilter />
-        <div className="articles-grid">
-          {/* Articles will be mapped here */}
+    <div className="min-h-screen bg-gray-50">
+      {/* Hero Section with Navbar overlay */}
+      <div className="relative">
+        <HeroCarousel articles={articles} />
+        <div className="absolute top-0 left-0 right-0 z-10">
+      <Navbar />
         </div>
-      </main>
-      <Sidebar />
+      </div>
+
+      {/* Main Content with 240px horizontal margin */}
+      <div className="mx-60">
+        <main className="py-8">
+          {/* Categories Section */}
+          <section className="mb-12">
+            <CategorySection 
+              selectedCategory={selectedCategory}
+              onCategorySelect={setSelectedCategory}
+              categories={categories}
+            />
+          </section>
+
+          <div className="grid grid-cols-1 lg:grid-cols-4 gap-8">
+            {/* Main Content */}
+            <div className="lg:col-span-3">
+              {/* Recent Articles Section */}
+              <section>
+                <RecentArticles 
+                  selectedCategory={selectedCategory} 
+                  articles={articles}
+                />
+              </section>
+            </div>
+
+            {/* Sidebar */}
+            <div className="lg:col-span-1">
+              <Sidebar />
+            </div>
+          </div>
+        </main>
+      </div>
     </div>
   );
+}
+
+// This function gets called at build time
+export async function getStaticProps() {
+  // Read the articles.json file
+  const articlesPath = path.join(process.cwd(), 'src', 'data', 'articles.json');
+  const articlesContents = fs.readFileSync(articlesPath, 'utf8');
+  const articlesData = JSON.parse(articlesContents);
+  
+  // Read the categories.json file
+  const categoriesPath = path.join(process.cwd(), 'src', 'data', 'categories.json');
+  const categoriesContents = fs.readFileSync(categoriesPath, 'utf8');
+  const categoriesData = JSON.parse(categoriesContents);
+  
+  // Pass data to the page via props
+  return { 
+    props: { 
+      articles: articlesData.articles,
+      categories: categoriesData.categories
+    } 
+  };
 } 
