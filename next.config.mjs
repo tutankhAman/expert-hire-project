bd725c5195cb9efa4eacf553d702ae1d97cadcb5
tutankhAman <132681496+tutankhAman@users.noreply.github.com>
2025-04-30 20:51:33 +0530

    ts initalisation

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,19 @@ const nextConfig = {
   images: {
     domains: [],
     unoptimized: true,
-  }
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: false,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: false,
+  },
 };
 
 export default nextConfig;
